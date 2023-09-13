c612487eef80743b3d2d5f43029a56a2805ea2e5
Marc Ferraro <marc.ferraro1@gmail.com>
2023-09-13 17:54:22 -0500

    implemented search fn and printing results

--- src/lib.rs
+++ src/lib.rs
@@ -23,12 +23,26 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    // println!("With text:\n{contents}");
+    
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    // returning in chapter 13 with improvements
+    // should this return an option to factor in missing?
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    
+    results
 }
 
 #[cfg(test)]
@@ -39,9 +53,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-            Rust:
-            Safe, fast, productive.
-            Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
         
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
