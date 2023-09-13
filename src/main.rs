8e01c633524c2aeb07eff0681d86ca4f04865dbc
Marc Ferraro <marc.ferraro1@gmail.com>
2023-09-13 16:20:22 -0500

    Config build / result handling + run fn

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for: \"{}\"", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file.");
-
-    println!("With text:\n{contents}");
+    run(config);
 }
 
 struct Config {
@@ -20,14 +20,25 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         // In future refactor, consider consuming args here, unlikely to resuse
+        // or find a way to not use clone()
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments")
         }
-        Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        }
+        
+        Ok(
+            Config {
+                query: args[1].clone(),
+                file_path: args[2].clone(),
+            }
+        )
     }
+}
+
+fn run(config: Config) {
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file.");
+
+    println!("With text:\n{contents}");
 }
