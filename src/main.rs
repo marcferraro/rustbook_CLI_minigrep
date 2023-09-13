92b3a02635c4a79230b738f760ab3d99fabced9e
Marc Ferraro <marc.ferraro1@gmail.com>
2023-09-13 16:43:45 -0500

    extracted definitions to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{env, fs, process, error::Error};
+use std::{env, process,};
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,38 +12,8 @@ fn main() {
     println!("Searching for: \"{}\"", config.query);
     println!("In file {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        // In future refactor, consider consuming args here, unlikely to resuse
-        // or find a way to not use clone()
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
-        
-        Ok(
-            Config {
-                query: args[1].clone(),
-                file_path: args[2].clone(),
-            }
-        )
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-        // .expect("Should have been able to read the file.");
-
-    println!("With text:\n{contents}");
-    Ok(())
 }
